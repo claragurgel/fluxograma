f83749819feac88ba4fca9a31dd80e8f323d7a65
Carolinne Rodrigues <carolinne.rodrigues@dtidigital.com.br>
2020-09-16 16:06:55 -0300

    Adiciona teste de fluxograma

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Stage, Layer, Rect, Line } from "react-konva";
 
-function App() {
+const App = () => {
+  const [exibirSim, setExibirSim] = useState(false);
+  const [exibirNao, setExibirNao] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="qualquer">
+          <h3>Você separa as águas do vaso sanitário das águas cinzas?</h3>
+        </div>
+        <div class="row">
+          <button
+            type="button"
+            class="btn btn-primary"
+            onClick={() => setExibirSim(true)}
+            style={{ marginTop: "20px", marginRight: "15px" }}
+          >
+            Sim
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            onClick={() => setExibirNao(true)}
+            style={{ marginTop: "20px" }}
+          >
+            Não
+          </button>
+        </div>
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+          {exibirSim && (
+            <Layer>
+              <Line
+                x={250}
+                y={200}
+                points={[400, -200, 400, -50, -50, -50]}
+                stroke="#A34700"
+              />
+
+              <Rect
+                x={100}
+                y={100}
+                width={100}
+                height={100}
+                fill="#704E25"
+                shadowBlur={5}
+              />
+            </Layer>
+          )}
+          {exibirNao && (
+            <Layer>
+              <Line
+                x={250}
+                y={200}
+                points={[470, -200, 470, -50, 1000, -50]}
+                stroke="#A34700"
+              />
+              <Rect
+                x={1200}
+                y={100}
+                width={100}
+                height={100}
+                fill="#704E25"
+                shadowBlur={5}
+              />
+            </Layer>
+          )}
+        </Stage>
       </header>
     </div>
   );
-}
+};
 
 export default App;
